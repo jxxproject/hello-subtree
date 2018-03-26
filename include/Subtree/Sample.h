c6e7aa5e6f8980647cea389d8becb7d8954c6f00
Michael Steffens <michael_steffens@posteo.de>
2018-03-26 13:16:44 +0200

    Populate subtree.

---
+++ include/Subtree/Sample.h
@@ -0,0 +1,12 @@
+#ifndef Subtree_Sample_INCLUDED
+#define Subtree_Sample_INCLUDED
+
+namespace Subtree {
+
+	class Sample
+	{
+	};
+
+} // namespace Subtree
+
+#endif // Subtree_Sample_INCLUDED
---
+++ test/sampleTest.cpp
@@ -0,0 +1,6 @@
+#include "Subtree/Sample.h"
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
